acbd02bb7c4f6a658495ba5a392eae3382466498
Temistocles Zwang <temis2st@gmail.com>
2023-08-10 17:57:38 -0300

    add query string

--- nestjs-mvc/src/app.controller.ts
+++ nestjs-mvc/src/app.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res, Query,Render } from '@nestjs/common';
 import { Response } from 'express';
 import * as path from 'path';
 import * as exphbs from 'express-handlebars';
 
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-
-
-
-
 @Controller()
 export class AppController {
   @Get('/')
-  async getHello(@Res() res: Response): Promise<void> {
-    const viewEngine = exphbs.create({ extname: '.hbs' });
+  @Render('index')
+  async getHello(): Promise<{ message: string }> {
+    return { message: 'Hello World' };
+  }
 
-    const viewPath = path.join(__dirname, '..', 'views'); // Assuming views folder is at the root level
-    
-    res.render(path.join(viewPath, 'index'), { 
-      layout: false, // Disable layout
-      message: 'Hello World' 
-    });
+  @Get('/teste')
+  @Render('index')
+  async getTeste(@Query('param') param: string): Promise<{ message: string }> {
+    return { message: `Teste com parâmetro: ${param}` };
   }
 }
-
