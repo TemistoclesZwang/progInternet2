bee6413f6675f8f0a6d07f3802cfbe0fa45f5eb8
Temistocles Zwang <temis2st@gmail.com>
2023-10-01 08:01:30 -0300

    invertida ordem dos cards

--- up-down/src/pages/components/TaskList/index.tsx
+++ up-down/src/pages/components/TaskList/index.tsx
@@ -1,13 +1,14 @@
 import { Topic } from '../NewTopic';
 import { ButtonUpDown } from '../ButtonUpDown';
 import { TotalVotes } from '../TotalVotes';
-import './index.css'
+import './index.css';
 import { useState, useEffect } from 'react';
-
 
 export function TopicList() {
     const [topics, setTopics] = useState<Topic[]>([]);
-    const [votes, setVotes] = useState<{ [id: string]: { up: number; down: number } }>({});
+    const [votes, setVotes] = useState<{
+        [id: string]: { up: number; down: number }
+    }>({});
 
     const handleUpVote = (id: string) => {
         setVotes((prevVotes) => ({
@@ -33,30 +34,37 @@ export function TopicList() {
         fetch('http://localhost:3000/topics')
             .then((response) => response.json())
             .then((data) => {
-                setTopics(data|| []);
+                setTopics(data.reverse() || []);
             })
             .catch((error) => {
                 console.error('Erro ao obter os tópicos:', error);
             });
-    }, []); 
+    }, []);
+
     return (
         <>
             <div className="cardTopicList">
-                {topics && topics.map((topic) => ( 
+                {topics && topics.map((topic) => (
                     <div key={topic.id} className="card">
                         <div className="containerTxt">
                             <p className="descriptionTxt">
-                                Descrição:<span className="boldTxt">{topic.description}</span>
+                                Descrição:<span className="boldTxt">{
+                                    topic.description
+                                }</span>
                             </p>
                             <p className="autorTxt">Autor:{topic.autor.nome}</p>
-                            <p className="dataTxt">Data:{new Date(topic.created_at).toLocaleString()}</p>
+                            <p className="dataTxt">Data:{
+                                new Date(topic.created_at).toLocaleString()
+                            }</p>
                         </div>
                         <ButtonUpDown
                             onUpClick={() => handleUpVote(topic.id)}
                             onDownClick={() => handleDownVote(topic.id)}
                         />
                         {votes[topic.id] && (
-                            <TotalVotes totalUp={votes[topic.id].up} totalDown={votes[topic.id].down} />
+                            <TotalVotes totalUp={votes[topic.id].up} totalDown={
+                                votes[topic.id].down
+                            } />
                         )}
                     </div>
                 ))}
