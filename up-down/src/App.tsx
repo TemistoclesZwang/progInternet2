1f725c3efae5c77367219308079f41d4c66fe726
Temistocles Zwang <temis2st@gmail.com>
2023-10-09 13:43:31 -0300

    movida paginas

--- up-down/src/App.tsx
+++ up-down/src/App.tsx
@@ -1,39 +1,13 @@
-import React, { useEffect, useReducer } from 'react';
 import './App.css';
-import { NewTopic, Topic } from './components/NewTopic';
-import { TopicListWithReducer } from './components/TopicList';
-import { ActionType, TopicReducer } from './reducers/TopicListReducer';
-import { fetchTopics, addTopic } from './services/api';
+import { IndexPage } from './pages';
 
 function App() {
-    const [{ topics }, dispatch] = useReducer(TopicReducer, { topics: [] })
 
-    useEffect(() => {
-        fetchTopics()
-            .then(data => {
-                dispatch({ type: ActionType.LOADED, payload: { topic: data } })
-            })
-            .catch(error => {
-                console.error('Erro ao carregar tópicos:', error);
-            });
-    }, [])
-
-    const handleAddTopic = async (newTopic: Topic) => {
-        try {
-            await addTopic(newTopic);
-            dispatch({ type: ActionType.ADDED_TOPIC, payload: { topic: newTopic } });
-            window.location.reload();
-        } catch (error) {
-            console.error('Erro ao adicionar tópico:', error);
-        }
-    }
 
     return (
-        <main className='container'>
-            <h1>Lista de Tópicos</h1>
-            <NewTopic onAddTopic={handleAddTopic} />
-            <TopicListWithReducer />
-        </main>
+<>
+<IndexPage></IndexPage>
+</>
     );
 }
 
