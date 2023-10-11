08105baa4ef25bb114c24e2b62606076e8e63384
Temistocles Zwang <temis2st@gmail.com>
2023-10-11 19:22:25 -0300

    ajustado routerProvider na main

--- up-down/src/App.tsx
+++ up-down/src/App.tsx
@@ -1,22 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-
-import { IndexPage } from './pages/home';
-import { LoginForm } from './pages/login';
-import { NotFound } from './pages/NotFound';
+import { About } from './pages/about';
 
 function App() {
     return (
         <BrowserRouter>
-            <nav>
+            <nav className='links'>
                 <Link to="/login">Login</Link>
-                <Link to="/">Home</Link>
+                <Link to="/topics">Tópicos</Link>
+                <Link to="/about">Sobre</Link>
             </nav>
-            <Routes>
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/topics" element={<IndexPage />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
         </BrowserRouter>
     );
 }
