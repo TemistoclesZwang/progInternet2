4bf50c4324c5e7fe07ac3c892c6ff3a9e286d2a8
Temistocles Zwang <temis2st@gmail.com>
2023-11-02 14:50:47 -0300

    conexão com BD feita

--- tasks-api-orm/src/data-source.ts
+++ tasks-api-orm/src/data-source.ts
@@ -1,17 +1,26 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+require('dotenv').config();
 
+
+
+console.log('Nome banco',process.env.DB_USERNAME)
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: true,
     entities: [User],
-    migrations: [],
     subscribers: [],
-})
+    migrations: ["src/**/migration/*.{js,ts}"],
+  });
+  
+  AppDataSource.initialize()
+    .then(() => console.log("Database UP - OK"))
+    .catch(console.error);
